6479f03bbdbf6d9114bcad70cbc3e618f8e92c29
George Paraskevopoulos <georgepar@gmail.com>
2015-10-27 14:04:29 +0000

    fix environment

--- config/create_config_json.py
+++ config/create_config_json.py
@@ -4,19 +4,26 @@ import pprint
 import sys
 
 def main(bonfire_home, config_file):
-    new_conf={}
-    os.system('source {0}/bin/env'.format(bonfire_home))
-    fd = open(config_file)
-    input_conf=json.load(fd)
-    pprint.pprint(input_conf)
-    fd.close()
-    new_conf=input_conf
-    for k,v in input_conf['env'].items():
-        print 'key = {0}, val = {1}'.format(k, os.environ.get(k, ''))
-        new_conf['env'][k] = os.environ.get(k, '')
-    fd2 = open(config_file, 'w')
-    json.dump(new_conf, fd2, indent=4)
 
+    with open(config_file) as fd:
+        input_conf=json.load(fd)
+    pprint.pprint(input_conf)
+ 
+    bonfire_env = {}
+    with open('{0}/setup_env_vars.sh'.format(bonfire_home)) as fd:
+        for line in fd:
+            if 'PATH' not in line:
+                var = line.split(' ')[-1]
+                k, v = var.split('=')
+                bonfire_env[k] = v.strip()
+
+    for k,v in bonfire_env.items():
+        print 'key = {0}, val = {1}'.format(k, v)
+        input_conf['env'][k] = v
+ 
+    out = json.dumps(input_conf, indent=4, sort_keys=True)
+    with open(config_file, 'w') as fd:
+        fd.write(out)
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
