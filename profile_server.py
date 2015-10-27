79999340712cd5a206be1580bc8397a0188c6ed2
George Paraskevopoulos <georgepar@gmail.com>
2015-10-27 13:45:51 +0000

    fixed bugs and linting done

--- profile_server.py
+++ profile_server.py
@@ -18,31 +18,31 @@ profiler_list = None
 profiler_id1 = 0
 
 
-
 def init_env(env_conf):
-    #recieves the initial opts and starts the deamon
-    for key,value in env_conf:
+    # recieves the initial opts and starts the deamon
+    for key, value in env_conf:
         if key in os.environ:
-            os.environ[key]+=' '+value
+            os.environ[key] += ' ' + value
         else:
-            os.environ[key]=value
+            os.environ[key] = value
 
 
 @bottle.route('/init/<profiler>', method='POST')
 def init(profiler):
     global profiler_list
     global profiler_id1
-    #receives the initial opts and starts the deamon
+    # receives the initial opts and starts the deamon
     profiler_conf = bottle.request.json
     profiler_list.append(profilers[profiler](profiler_conf))
     profiler_id1 += 1
-    response.data = profiler_id1
-    #here we send the profiler_id
-    return bottle.HTTPResponse(status=200, body=bod)
+    #bottle.response.data = profiler_id1
+    # here we send the profiler_id
+    return bottle.HTTPResponse(status=200, body=profiler_id1)
+
 
 @bottle.route('/start/<profiler_id>', method='POST')
 def start(profiler_id):
-    #in order to start a recording we get an opts argument
+    # in order to start a recording we get an opts argument
     profiler_list[profiler_id].setup_karaf_env()
     profiler_list[profiler_id].start()
 
@@ -56,19 +56,20 @@ def stop(profiler_id):
 def get_data(profiler_id):
     opts = profiler_list[profiler_id].opts
     return bottle.static_file(opts['recording_file'], opts['output_dir'])
-    #here is the endpointa
+    # here is the endpointa
 
 
 @bottle.route('/get_flamegraph/<profiler_id>', method='POST')
 def get_flamegraph(profiler_id):
-    #here we should get the binary blobs
+    # here we should get the binary blobs
     opts = profiler_list[profiler_id].opts
     return bottle.static_file(opts['flamegraph_name'], opts['output_dir'])
 
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument('--json-config',
                         required=True,
@@ -77,10 +78,9 @@ if __name__ == '__main__':
                         action='store',
                         help='Configuration file (JSON)')
 
-
     args = parser.parse_args()
     conf = {}
     with open(args.json_config) as conf_file:
-      conf = json.load(conf_file)
-    init_env(conf_file[env_conf])
+        conf = json.load(conf_file)
+    init_env(conf['env'])
     bottle.run(host=conf['master_ip'], port=conf['master_port'], debug=True)
